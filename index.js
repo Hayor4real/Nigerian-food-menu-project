96711b9196f84503d73bc64c7ebcaf86393ce9fa
Akindele <AKINDELEAYORINDE55@GMAIL.COM>
2022-04-27 09:44:39 +0200

    Initial commits
    description

---
+++ index.js
@@ -0,0 +1,257 @@
+const menu = [
+    {
+      id: 1,
+      title: "Jollof-rice",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/Jollof-rice-recipe.webp",
+      desc: `Jollof rice is unarguably the most popular Nigerian food. I advise folks that are just starting with Nigerian cooking to start with jollof rice.
+     The recipe is easy, and the required ingredients could be found in any country in the world. If you could lay your hands on tomatoes and chicken, you could make jollof rice.`,
+    },
+    {
+      id: 2,
+      title: "Soup and Swallow",
+      category: "lunch",
+      price: 13.99,
+      img: "./images/egusi-soup-recipe.webp",
+      desc: `Nigerian swallows include eba, fufu, Semo, pounded yam, tuwo shinkafa and quite a few others. In Nigeria, we serve swallow and soups as dinner or lunch.
+     The most popular soup in Nigeria is egusi soup, here for Egusi Soup Recipe`,
+    },
+    {
+      id: 3,
+      title: " Porridge Yam",
+      category: "shakes",
+      price: 6.99,
+      img: "./images/yam-porridge-recipe.webp",
+      desc: `I prepare yams in different ways in Nigeria. 
+      Porridge yam is like the number one way to eat yam in Nigeria. It is a major staple food in Nigeria.`,
+    },
+    {
+      id: 4,
+      title: " Nigerian Noodles",
+      category: "breakfast",
+      price: 20.99,
+      img: "./images/indomie-noodles.webp",
+      desc: `Noodle is food to millions of Nigerians, especially kids and young adults. Millions of tonnes of noodles are consumed monthly in Nigerian universities.
+      I could make it in less than 10 minutes, so lots of students are depending on noodles to stay full. `,
+    },
+    {
+      id: 5,
+      title: "Nigerian Porridge Beans",
+      category: "lunch",
+      price: 22.99,
+      img: "./images/porridge-beans.webp",
+      desc: `Porridge beans is a popular Nigerian food. There is hardly any reputable eateries or restaurant in Nigerian that doesn’t serve cooked beans.
+           There are many ways to cook beans in Nigeria.
+           You could make beans and yam porridge combo or boil plain white beans and serve with stew.`,
+    },
+    {
+      id: 6,
+      title:  "Tea and Bread",
+      category: "shakes",
+      price: 18.99,
+      img: "./images/breakfast-ideas.webp",
+      desc: `Tea and bread is a popular delicious breakfast from Nigeria. 
+      Lots of Nigerians eat this for breakfast. Often, it is combined with fried eggs.
+      `,
+    },
+    {
+      id: 7,
+      title: " Spaghetti",
+      category: "breakfast",
+      price: 8.99,
+      img: "./images/Spaghetti.webp",
+      desc: `Spaghetti is a popular Nigerian food, It doesn’t stay much in the pot when I prepare it this way. My kids love spaghetti this way, their dad too. 😀
+      You could also boil spaghetti and serve with stew or sauce.`,
+    },
+    {
+      id: 8,
+      title: " Unripe Plantain Porridge",
+      category: "lunch",
+      price: 12.99,
+      img: "./images/plantain-porridge-1.webp",
+      desc: `Unripe plantain porridge is food to millions of Nigerians. 
+      I make this at least once every week in my home.`,
+    },
+    {
+      id: 9,
+      title: " Nigerian Vegetable soup",
+      category: "shakes",
+      price: 16.99,
+      img: "./images/vegetable-soup-2.webp",
+      desc: `I combined vegetable soup with eba or fufu in Nigeria and served as either lunch or dinner..`,
+    },
+    {
+      id: 10,
+      title: "Nigerian Fried Rice",
+      category: "dinner",
+      price: 22.99,
+      img: "./images/Fried-rice.webp",
+      desc: `Fried rice is a very popular Nigerian food. There is a twist to the Nigerian version. Several recipes exist for fried rice across the nations.
+    Most times we serve fried rice, chicken and vegetable salad together.`,
+    },
+    {
+      id: 11,
+      title: "Rice and vegetable Sauce",
+      category: "lunch",
+      price: 20.99,
+      img: "./images/Nigerian-vegetable-sauce.webp",
+      desc: `We eat lots of rice in Nigeria. There are over 20 different rice recipes from Nigeria. This is one of them.
+     It make separately the vegetable sauce and then served with boiled white rice.`,
+    },
+    {
+      id: 12,
+      title: " Moi Moi",
+      category: "breakfast",
+      price: 25.01,
+      img: "./images/moi-moi2.webp",
+      desc: `Moi moi (beans pudding) is a Nigerian food that is loved by many people from Nigeria, I could serve it with pap or custard for breakfast.
+      Moi moi can also be served as a main dish in Nigerian homes.`,
+    },
+    {
+      id: 13,
+      title: "Agidi (Eko)",
+      category: "shakes",
+      price: 15.21,
+      img: "./images/agidi.webp",
+      desc: `I like to combine agidi and cooked beans and then serve with stew as you can see above. Agidi (eko) is food for the Yorubas, other ethnic groups in Nigeria also have a way of making this.
+    It made Agidi from corn (maize), the Igbos also loved it.
+    Another recipe is what we call agidi Jollof..`,
+    },
+    {
+      id: 14,
+      title: " Nigerian Pepper Soup",
+      category: "dinner",
+      price: 20.27,
+      img: "./images/pepper-soup.webp",
+      desc: `Pepper soup is a popular food in Nigeria. 
+      I serve peppersoup as a side dish or combined with yam and served as lunch or dinner.`,
+    },
+    {
+      id: 15,
+      title: "Home Made Bread",
+      category: "dinner",
+      price: 24.76,
+      img: "./images/bread.webp",
+      desc: `Bread could be store-bought or homemade, 
+      we eat lots of bread in Nigeria. I serve as a snack or pair with tea and served as breakfast. Bread is a well know Nigerian food..`,
+    },
+    {
+      id: 16,
+      title: "Suya Nigerian food",
+      category: "dinner",
+      price: 20.27,
+      img: "./images/suya.webp",
+      desc: `I make Suya from beef or chicken. I have also seen fish suya and some other recipes. We eat lots of suya in Nigeria.
+      Usually as a snack or side dish..`,
+    },
+    {
+      id: 17,
+      title: "Yamarita",
+      category: "breakfast",
+      price: 15.27,
+      img: "./images/served-yamarita.webp",
+      desc: `Yamarita is food to millions of Nigerians. It is a very popular Hausa food. Unlike several other foods that take several hours to prepare, I could make this Nigerian food in just a few minutes.
+      It is just egg-coated yams that undergo frying..`,
+    },
+    {
+      id: 18,
+      title: "Rice and Tomato stew",
+      category: "lunch",
+      price: 30.27,
+      img: "./images/rice-and-tomato-stew.webp",
+      desc: `A combination of rice and tomato stew is a popular Nigerian food. I served it exactly the way you see above.
+      Most times I pour the stew on top of the rice instead of being on a separate plate.
+      Lots of Nigerians eat rice and stew for dinner.
+      Stew is a big word in Nigeria food-osphere. There are unique twists to it.`,
+    },
+    {
+      id: 19,
+      title: " African Salad",
+      category: "shakes",
+      price: 25.46,
+      img: "./images/Africa-salad.webp",
+      desc: `African salad is a popular Igbo food. 
+      The key ingredient is cassava flakes. The rest is palm oil, peppers, etc..`,
+    },
+    {
+      id: 20,
+      title: "dinner",
+      price: 35.49,
+      img: "./images/ogbono-soup1.webp",
+      desc: `Ogbono soup is certainly the second most popular soup in Nigeria. There is hardly any eatry or restaurant in Nigeria that doesn’t serve ogbono soup.
+    It is also easy to prepare.Ogbono soup can be served with either eba, fufu or semo..`,
+    },
+  ];
+
+
+
+const sectionCenter = document.querySelector(".section-center");
+const container = document.querySelector(".btn-container")
+
+window.addEventListener("DOMContentLoaded", function(){
+    displayMenuItems(menu);
+    displayMenuButtons();
+    
+})
+//filter items
+
+
+function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function(item){
+        
+        return `<article class="menu-item">
+        <img src=${item.img} class="photo" alt=${item.title} />
+        <div class="item-info">
+            <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">$${item.price}</h4>
+
+            </header>
+            <p class="item-text">
+               ${item.desc}
+            </p>  
+        </div>
+    </article>`
+    })
+    displayMenu = displayMenu.join("");
+    sectionCenter.innerHTML = displayMenu; 
+}
+
+function displayMenuButtons(){
+    const categories = menu.reduce(function(values, item){
+        if(!values.includes(item.category)){
+            values.push(item.category)
+            
+        }
+        return values
+    }, ["All"]
+    
+    );
+    const categoryBtns = categories.map(function(category){
+        return ` <button class="filter-btn" type="button" 
+        data-id=${category}>
+        ${category}</button>`
+        
+    }).join("");
+    container.innerHTML = categoryBtns;
+    const filterBtns = container.querySelectorAll(".filter-btn");
+    filterBtns.forEach(function(btn){
+        btn.addEventListener("click", function(e){
+            const category = e.currentTarget.dataset.id
+            const menuCategory = menu.filter(function(menuItem){
+                 if(menuItem.category === category) {
+                     return menuItem
+                 }
+            });
+            if(category === "All") {
+                displayMenuItems(menu)
+            }
+            else {
+              displayMenuItems(menuCategory )
+            }
+        })
+      })
+      
+
+}
